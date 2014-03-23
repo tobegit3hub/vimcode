5eff7e7596ebfded6e19a890de87af41c59b5233
tobe <tobegit3hub@gmail.com>
2014-03-23 20:52:10 +0800

    new static page controller and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Vimcode::Application.routes.draw do
+  get "static_page/home"
+
+  root "static_page#home"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
